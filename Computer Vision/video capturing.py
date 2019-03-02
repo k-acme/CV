9fca320da777ba9de5953e47ad554f3cfca42c0f
arbaz52 <arbaz5256@gmail.com>
2019-03-02 22:15:57 +0500

    Video Capturing and filtering now works fine...

--- Computer Vision/video capturing.py
+++ Computer Vision/video capturing.py
@@ -7,26 +7,26 @@ Created on Sat Mar  2 21:22:02 2019
 
 
 import cv2
+import numpy
+
+from PIL import Image as imagereader
+
 import image_filtering as imagefilter
 import image_sampling as imagesampler
 ver_filter = [[1,1,1], [0,0,0], [-1,-1,-1]]
 
 
-def display_image(imglist):
-    newimage = imagereader.fromarray(numpy.asarray(imglist))
-    #newimage.save(path+"temp.jpg")
-    newimage.show()
-    
 
 cap = cv2.VideoCapture(0)
 while(True):
     ret, frame = cap.read()
     gr = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    ratio = 5
-    gr_down_sampled = imagesampler.down_sample_2d(gr, len(gr)/ratio, len(gr[0])/ratio)
+    ratio = 3
+    gr_down_sampled = imagesampler.down_sample_2d(gr, len(gr[0])/ratio, len(gr)/ratio)
     verfilteredimagearr = imagefilter.filter_image(gr_down_sampled, ver_filter, 1)
-    #display_image(verfilteredimagearr)
-    cv2.imshow("frrame", gr_down_sampled)
+    framex = cv2.UMat(numpy.array(imagereader.fromarray(numpy.asarray(verfilteredimagearr)), dtype=numpy.uint8))
+    #framex = cv2.UMat(numpy.asarray(verfilteredimagearr))
+    cv2.imshow("IMAGE: ", framex)
     if cv2.waitKey(1) & 0xFF == ord("q"):
         #out = cv2.imwrite('capture.jpg', frame)
         break
